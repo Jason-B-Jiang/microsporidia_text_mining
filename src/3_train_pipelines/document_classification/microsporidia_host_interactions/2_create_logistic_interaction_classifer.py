ff395b684c513e66a357b7e3cf0ae0ec5f70c995
Jason-B-Jiang <jasonbitan.jiang@mail.utoronto.ca>
2022-11-14 14:29:55 -0500

    set up 10-fold validated logistic regression + baseline model for positive/negative parasite-host interactions

--- src/3_train_pipelines/document_classification/microsporidia_host_interactions/2_create_logistic_interaction_classifer.py
+++ src/3_train_pipelines/document_classification/microsporidia_host_interactions/2_create_logistic_interaction_classifer.py
@@ -4,7 +4,7 @@
 # host interactions
 #
 # Jason Jiang - Created: 2022/11/10
-#               Last edited: 2022/11/10
+#               Last edited: 2022/11/14
 #
 # Mideo Lab - Microsporidia text mining
 #
@@ -29,14 +29,94 @@ labels = np.array([int(entry[1]) for entry in labelled_dataset])
 
 ###############################################################################
 
-# Set up 5-fold 80:20 train-test split for our dataset, and train a logistic
-# model for each split
-kf = KFold(n_splits=5)
-i = 1
+## Rule-based baseline
+
+def naive_classifier(x: np.ndarray) -> np.ndarray:
+    # For a 2D array of candidate word appearances in text, return a 1D array
+    # with 1 or 0 for each original row
+    #
+    # 1 = >=1 candidate word appears in text
+    # 0 = no candidate words appear in text
+    return np.sum(x, axis=1).astype(dtype=bool).astype(dtype=int)
+
+###############################################################################
+
+## Set up 10-fold cross-validation training/testing sets for our dataset
+
+kf = KFold(n_splits=10, shuffle=True, random_state=42)
+kf_split = tuple(kf.split(labelled_dataset))
+
+###############################################################################
+
+## Evaluate our rule-based baseline for classifying positive/negative documents
+
+precision_pos = []
+precision_neg = []
+recall_pos = []
+recall_neg = []
+f1_pos = []
+f1_neg = []
+
+for train, test in kf_split:
+    # only need test data, as we don't need to train our rule-based baseline
+    test_data, test_labels = data[test], labels[test]
+    naive_preds = naive_classifier(test_data)
+
+    # create zipped list, where each tuple has predicted label and actual label
+    zipped_pred_and_labels = list(zip(list(test_labels), list(naive_preds)))
+
+    # positives:
+    # tp: label = 1 and pred = 1
+    # fp: label = 0 and pred = 1
+    # fn: label = 1 and pred = 0
+    tp_pos = np.sum([x[0] == 1 and x[1] == 1 for x in zipped_pred_and_labels])
+    fp_pos = np.sum([x[0] == 0 and x[1] == 1 for x in zipped_pred_and_labels])
+    fn_pos = np.sum([x[0] == 1 and x[1] == 0 for x in zipped_pred_and_labels])
+
+    precision_pos_ = (tp_pos / (tp_pos + fp_pos)) * 100
+    recall_pos_ = (tp_pos / (tp_pos + fn_pos)) * 100
+
+    precision_pos.append(precision_pos_)
+    recall_pos.append(recall_pos_)
+    f1_pos.append(2 * ((precision_pos_ * recall_pos_) / (precision_pos_ + recall_pos_)))
+
+    # negatives:
+    # tp: label = 0 and pred = 0
+    # fp: label = 1 and pred = 0
+    # fn: label = 0 and pred = 1
+    tp_neg = np.sum([x[0] == 0 and x[1] == 0 for x in zipped_pred_and_labels])
+    fp_neg = np.sum([x[0] == 1 and x[1] == 0 for x in zipped_pred_and_labels])
+    fn_neg = np.sum([x[0] == 0 and x[1] == 1 for x in zipped_pred_and_labels])
+
+    precision_neg_ = (tp_neg / (tp_neg + fp_neg)) * 100
+    recall_neg_ = (tp_neg / (tp_neg + fn_neg)) * 100
+
+    precision_neg.append(precision_neg_)
+    recall_neg.append(recall_neg_)
+    f1_neg.append(2 * ((precision_neg_ * recall_neg_) / (precision_neg_ + recall_neg_)))
+
+baseline_performance_df = dict(pd.DataFrame({
+    'precision_pos': precision_pos,
+    'precision_neg': precision_neg,
+    'recall_pos': recall_pos,
+    'recall_neg': recall_neg,
+    'f1_pos': [0.0 if np.isnan(f1) else f1 for f1 in f1_pos],
+    'f1_neg': [0.0 if np.isnan(f1) else f1 for f1 in f1_neg]
+}).mean())
+
+
+###############################################################################
+
+## Logistic regression models for predicting positive/negative texts
+
+# Set up class weights for positive and negative classes for parameter grid
+# search
+param_grid = [{0: 8.0, 1: 1.0}, {0: 4.0, 1: 1.0}, {0: 2.0, 1: 1.0},
+              {0: 1.0, 1: 1.0}, {0: 1.0, 1: 2.0}, {0: 1.0, 1: 4.0},
+              {0: 1.0, 1: 8.0}]
 
 # initialize columns for dataframe of model performance metrics
-model_name = []
-overall_score = []
+class_weights = []
 n_pos = []
 n_neg = []
 precision_pos = []
@@ -46,35 +126,39 @@ recall_neg = []
 f1_pos = []
 f1_neg = []
 
-for train, test in kf.split(labelled_dataset):
-    train_data, train_labels = data[train], labels[train]
-    test_data, test_labels = data[test], labels[test]
+for param in param_grid:
+    i = 1
 
-    model = LogisticRegression(solver='liblinear', random_state=42).fit(train_data,
-                                                                        train_labels)
+    # ensure we use the same 10-fold dataset split when testing each set of
+    # class weights
+    for train, test in kf_split:
+        train_data, train_labels = data[train], labels[train]
+        test_data, test_labels = data[test], labels[test]
 
-    model_stats = classification_report(test_labels, model.predict(test_data),
-                                        output_dict=True)
+        model = LogisticRegression(solver='liblinear', random_state=42,
+                                   class_weight=param).fit(train_data,
+                                                           train_labels)
 
-    model_name.append(f"model_{i}")
-    overall_score.append(model.score(test_data, test_labels))
-    n_pos.append(model_stats['1']['support'])
-    n_neg.append(model_stats['0']['support'])
-    precision_pos.append(model_stats['1']['precision'])
-    precision_neg.append(model_stats['0']['precision'])
-    recall_pos.append(model_stats['1']['recall'])
-    recall_neg.append(model_stats['0']['recall'])
-    f1_pos.append(model_stats['1']['f1-score'])
-    f1_neg.append(model_stats['0']['f1-score'])
+        model_stats = classification_report(test_labels, model.predict(test_data),
+                                            output_dict=True)
 
-    i += 1
+        class_weights.append(f"negative: {param[0]}, positive: {param[1]}")
+        n_pos.append(model_stats['1']['support'])
+        n_neg.append(model_stats['0']['support'])
+        precision_pos.append(model_stats['1']['precision'])
+        precision_neg.append(model_stats['0']['precision'])
+        recall_pos.append(model_stats['1']['recall'])
+        recall_neg.append(model_stats['0']['recall'])
+        f1_pos.append(model_stats['1']['f1-score'])
+        f1_neg.append(model_stats['0']['f1-score'])
+
+        i += 1
 
 ###############################################################################
 
 # Create dataframe of performance metrics for each logistic model
 model_performance_df = pd.DataFrame({
-    'model': model_name,
-    'overall_score': overall_score,
+    'class_weights': class_weights,
     'n_pos': n_pos,
     'n_neg': n_neg,
     'precision_pos': precision_pos,
@@ -85,10 +169,16 @@ model_performance_df = pd.DataFrame({
     'f1_neg': f1_neg
 })
 
-# TODO:
-# 1) get seaborn plots ready
-# 2) look into accounting for class imbalances
-#    https://www.analyticsvidhya.com/blog/2020/10/improve-class-imbalance-class-weights/
-# 3) check vocab overlap between text corpus and our different models
-# 4) if logistic regression works better after accounting for class imbalances,
-#    look into random forest classifiers
+# Dataframe for average model performance for each set of class weights
+param_rankings = {'negative: 8.0, positive: 1.0': 0,
+                  'negative: 4.0, positive: 1.0': 1,
+                  'negative: 2.0, positive: 1.0': 2,
+                  'negative: 1.0, positive: 1.0': 3,
+                  'negative: 1.0, positive: 2.0': 4,
+                  'negative: 1.0, positive: 4.0': 5,
+                  'negative: 1.0, positive: 8.0': 6}
+
+avg_model_performance = \
+    model_performance_df.groupby(['class_weights']).mean().reset_index().sort_values(
+        by='class_weights', key=lambda col: col.map(param_rankings)
+    )

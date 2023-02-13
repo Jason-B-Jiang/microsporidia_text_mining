67cc5963c988ac71830804f5e1c2a7215894971c
Jason-B-Jiang <jasonbitan.jiang@mail.utoronto.ca>
2023-02-13 17:27:48 -0500

    continue refining polar tube coil rules

--- src/2_predict_traits/predict_polar_tube_coils/predict_polar_tube_coil.py
+++ src/2_predict_traits/predict_polar_tube_coils/predict_polar_tube_coil.py
@@ -3,7 +3,7 @@
 # Predict microsporidia polar tube coil measures from texts
 #
 # Jason Jiang - Created: Feb/07/2023
-#               Last edited: Feb/08/2023
+#               Last edited: Feb/13/2023
 #
 # Mideo Lab - Microsporidia text mining
 #
@@ -29,15 +29,18 @@ EXAMPLES = [
     """A new microsporidian species was described from the hypoderm of Daphnia magna sampled from gibel carp (Carassius auratus gibelio) ponds located in Wuhan city, China. The infected cladocerans generally appeared opaque due to numerous plasmodia distributed in the host integument. The earliest stages observed were uninucleate meronts that were in direct contact with the host cell cytoplasm. Meronts developed into multinucleate sporogonial plasmodia enclosed in sporophorous vesicles. Sporoblasts were produced by the rosette-like division of sporogonial division. Mature spores were pyriform and monokaryotic, measuring 4.48 ± 0.09 (4.34-4.65) µm long and 2.40 ± 0.08 (2.18-2.54) µm wide. The polaroplast was bipartite with loose anterior lamellae and tight posterior lamellae. Polar filaments, arranged in two rows, were anisofilar with two wider anterior coils, and five narrower posterior coils. The exospore was covered with fibrous secretions and was composed of four layers. Phylogenetic analysis based on the obtained SSU rDNA sequence, indicated that the present species clustered with three unidentified Daphnia pulicaria-infecting microsporidia with high support values to form a monophyletic lineage, rather than with the congener, Agglomerata cladocera. The barcode motif of the internal transcribed spacer (ITS) region of the novel species was unique among representatives of the "Agglomeratidae" sensu clade (Vávra et al., 2018). Based on the morphological characters and SSU rDNA-inferred phylogenetic analyses, a new species was erected and named as Agglomerata daphniae n. sp. This is the first report of zooplankton-infecting microsporidia in China.""",
     """Agglomerata lacrima n. sp. is the first species of the genus described from a copepod host (Acanthocyclops vernalis.) It was studied using light- and electron-microscopy. All stages of the life-cycle had isolated nuclei. The earliest stages found were uninucleate merozoites. Sporogony produced 4-12 (mostly 8) pyriform spores by rosette-like budding within a fragile sporophorous vesicle. Live spores measured 4.4 +/- 0.2 x 2.6 +/- 0.2 μm, and fixed spores measured 3.7 +/- 0.2 x 1.6 +/- 0.2 μm. The exospore was constructed of 4 layers. The anisofilar polar filament made 5-6 coils in the posterior half of the spore. The polaroplast had an anterior part with wide lamellae (chambers), followed by a second zone of narrow lamellae. Tubule-like structures which might constitute a third polaroplast region were present immediately anterior to the first filament coil. Cytological characteristics and the generic position of the species are discussed, and it is compared to related or resembling species, and to all previously reported microsporidian species from copepods.""",
     """The new microsporidium Agglomerata volgensae n. sp. is described based on light microscopic and ultrastructural characteristics. The parasite invades the hypoderm and adipose tissue of Daphnia magna and causes hypertrophy. All life cycle stages have isolated nuclei. Sporogonial plasmodia divide by rosette-like division, producing 4-16, usually 8, sporoblasts. A sporophorous vesicle is initiated after the first nuclear division of the sporont. The fragile vesicle either collects all daughter cells of the sporont, or the vesicle divides together with the plasmodium to enclose spores in individual sporophorous vesicles. Fibril-like projections connect the exospore with the envelope of the sporophorous vesicle. Tubules, with walls of exospore material, are formed together with the sporoblasts. Mature spores are pyriform with pointed anterior pole and an obliquely positioned posterior vacuole. Unfixed spores measure 3.2-3.7x1.7-2.0 μm. The exospore is layered, approximately 38 nm thick. The polar filament is lightly anisofilar with 2-3 wide anterior coils, and 2-3 more narrow posterior coils, in a single layer of coils in the posterior half of the spore. The polaroplast has two regions: irregular wide lamellae or chambers surrounding the straight part of the filament, and more loosely arranged narrow lamellae in the coil region. The discrimination from other microsporidian species and the systematic position are briefly discussed.""",
-    """On the basis of electronmicroscopic data the description of Alfvenia ceriodaphniae sp. n. from Ceriodaphnia reticulata (Crustacea, Cladocera) is presented. The nuclear apparatus of meronts is diplokariotic. Sporonts, sporoblasts and spores are uninuclear. The sporogonial Plasmodium undertakes the rosette-like division with the formation of sporoblasts. Spores are egg-shaped, their size: 4.5(4.2-4.8) X 3.3(2.9-3.5) ц т . Evry spore lays individually inside the envelope, that is formed from the external layer of the sporoblast wall. Polar tube is isofillar, forming 8 - 9 coils. Polaroplast is consisted of the lamellar and chamber parts. The site of parasite localisation is crustacean hypoderm."""
+    """On the basis of electronmicroscopic data the description of Alfvenia ceriodaphniae sp. n. from Ceriodaphnia reticulata (Crustacea, Cladocera) is presented. The nuclear apparatus of meronts is diplokariotic. Sporonts, sporoblasts and spores are uninuclear. The sporogonial Plasmodium undertakes the rosette-like division with the formation of sporoblasts. Spores are egg-shaped, their size: 4.5(4.2-4.8) X 3.3(2.9-3.5) ц т . Evry spore lays individually inside the envelope, that is formed from the external layer of the sporoblast wall. Polar tube is isofillar, forming 8 - 9 coils. Polaroplast is consisted of the lamellar and chamber parts. The site of parasite localisation is crustacean hypoderm.""",
+    # weird examples start here
+    """A new microsporidian parasite of a freshwater cladoceran from southern Sweden is described using light and electron microscopical methods. Development comprises 2 merogonial sequences, the first resulting in a cluster of 8 merozoites, the second in a chain of 4 merozoites. Each secondary merozoite develops into a sporont which divides into 2 sporoblasts, each of which develops into a spore. The spores are broadly oval and in fresh smears measure about 6 fim in length, with a single nucleus and a posterosome. The polar filament is about 40 fim long, of even thickness throughout, and appears as 15—18 coils in a single layer. The anchoring disc is small and the polaroplast is composed of 2 lamellar parts. Outside the plasma membrane of the sporont a 5-layered, electrondense substance is produced, which further differentiates into endo- and exospore, an electron-dense substance occurring patchily on the exospore and a pansporoblast membrane. During development the sporoblasts and the young spores are connected by a dense substance. Mature spores appear single or paired. The pansporoblast membrane is composed of 2 structurally different layers, namely a thin outer, single membrane and an inner layer composed of tubular structures. It is connected to the spore coat by patches of the dense substance. The new microsporidium is considered to belong to a new genus of the family Telomyxidae, and its systematic relationship with this and the related family Tuzetiidae is discussed. A survey of microsporidia from Cladocera is included.""",
+    """Canningia spinidentis gen. Et sp. n. infects the fir bark beetle Pityokteines spinidens Rtt. In Austria. The pathogen attacks mainly the fat body, Malpighian tubules, the muscles and the connective tissue of larvae and adults, and the gonads of adults. The development is haplokaryotic, with single spores. Spores are short tubular, uninucleate with globular anchoring disc inserted subapically, laterally, in a depression of the endospore wall. Polar filament is isofilar with 5/6 coils. Polaroplast is composed of two lamellar parts of different density. A new genus Canningia gen. n. is proposed based on differences in ultrastructures of spores from Unikaryon Canning, Barker, Hammond et Nicholas, 1974.""",
 ]
 
 matcher = Matcher(nlp.vocab)
 
 coil_pattern = [
-    {'LIKE_NUM': True},
+    {'POS': 'NUM'},
     {'TEXT': {'REGEX': ".+"}, 'OP': '{,3}'},
-    {'LIKE_NUM': True, 'OP': '?'},
+    {'POS': 'NUM', 'OP': '?'},
     {'TEXT': {'REGEX': ".+"}, 'OP': '{,3}'},
     {'LEMMA': {'REGEX': '(coil|spire|twist|turn)'}}
 ]
@@ -72,6 +75,10 @@ def predict_polar_tube_measures(text: str) -> List[np.ndarray]:
 
 
 def get_sentence_coil_measure(sent: spacy.tokens.span.Span) -> np.ndarray:
+    # corner case: uncoiled polar tube = 0 coils
+    if re.search("(uncoiled|noncoiled|non-coiled)", sent.text.lower()):
+        return np.array([0])
+
     # get polar coil pattern matches within sentence
     matches = matcher(sent)
 
@@ -79,8 +86,8 @@ def get_sentence_coil_measure(sent: spacy.tokens.span.Span) -> np.ndarray:
     # and only keeping the longest one
     resolved_matches = resolve_overlapping_matches(matches)
 
-    # return matches for polar coil measures as a tuple of integers
-    # length 1 tuple if a single value, length 2 if a range of coils is given
+    # return matches for polar coil measures as an array of floats
+    # length 1 array if a single value, length 2 if a range of coils is given
     return format_coil_matches_as_numbers(resolved_matches, sent)
 
 
@@ -100,16 +107,50 @@ def format_coil_matches_as_numbers(resolved_matches: List[Tuple[int]],
     coil_measures = []
 
     for match in resolved_matches:
-        sent_match = sent[match[1] : match[2]]
+        coil_measure = convert_coil_measure_to_numeric(sent[match[1] : match[2]])
 
-        coil_measure = \
-            [float(tok._.numerized) for tok in sent_match if tok.pos_ == 'NUM']
+        # a half-coil was indicated as a fraction, so add the fractional part
+        # to the whole coil count
+        if len(coil_measure) == 2 and coil_measure[1] < 1.0:
+            coil_measure = np.array([coil_measure[0] + coil_measure[1]])
 
-        coil_measures.append(np.array(coil_measure))
+        coil_measures.append(coil_measure)
 
     return sum(coil_measures)
+
+
+def convert_coil_measure_to_numeric(match: spacy.tokens.span.Span) -> np.ndarray:
+
+    numeric_coil_measures = []
+
+    for measure in [tok for tok in match if tok.pos_ == 'NUM']:
+        try:
+            numeric_coil_measure = float(measure._.numerized)
+
+        except ValueError:
+            
+            if '/' in measure.text:
+                 # fraction was not properly numerized
+                numeric_coil_measure = \
+                    int(match.text.split("/")[0]) / int(match.text.split("/")[1])
+
+            elif re.search('\\d+.\\d+', measure.text):
+                # some kind of weirdly delimited coil range
+                split_ = re.split('[^0-9\\.]', measure.text)
+                numeric_coil_measure = \
+                    np.array([numerizer.numerize(split_[0]), numerizer.numerize(split_[1])])
+            
+            else:
+                numeric_coil_measure = None
+        
+        if numeric_coil_measure is not None:
+            numeric_coil_measures.append(numeric_coil_measure)
+    
+    return np.array(numeric_coil_measures)
 
 ###############################################################################
 
 if __name__ == '__main__':
     main()
+
+# predict_polar_tube_measures("The polar tube had 6 - 7 coils. The other polar filament appears as 15—18 coils in a single layer. A rare polar tubule had 7 posterior coils and 2 to 5 anterior coils. I also found a polar tube that was uncoiled.")
